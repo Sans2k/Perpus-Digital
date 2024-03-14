bafc87c8357c74546f6b92ed9a6055f28f857f17
Sans2k <140954881+Sans2k@users.noreply.github.com>
2024-03-14 14:29:28 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row p-3 bg-primary text-white">
+    <div>
+      <img src="~/assets/img/LOGO-SMK4.png" alt="logo SMKN 4">
+    </div>
+
+    <div class="col-lg-5">
+      <h1>Perpus Digital</h1>
+      <address>
+        SMKN 4 Tasikmalaya
+        JL.Depok, Sukamenak, Purbaratu
+      </address>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+img {
+  width: 120px;
+  margin-left: 90%;
+}
+</style>
---
+++ pages/pengunjung/riwayat.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-lg-12">
+        <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
+        <div class="my-3">
+          <input type="search" class="form-control form-control-lg rounded-5" placeholder="Filter...">
+        </div>
+        <div class="my-3 text-muted">menampilkan 1 dari 1</div>
+        <table class="table">
+          <thead>
+            <tr>
+              <td>#</td>
+              <td>NAMA</td>
+              <td>KEANGGOTAAN</td>
+              <td>WAKTU</td>
+              <td>KEPERLUAN</td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1.</td>
+              <td>Zul Hilmi</td>
+              <td>Guru</td>
+              <td>24 Februari 2024, 23.31.00</td>
+              <td>Baca</td>
+            </tr>
+          </tbody>
+        </table>
+        <nuxt-link to="/menu" class="btn btn-dark btn-lg rounded-5 px-5">Kembali ke menu</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
