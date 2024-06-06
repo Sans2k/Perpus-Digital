672d097d1998c3715fc8fbe3ba9c8915794cd51a
Sans2k <140954881+Sans2k@users.noreply.github.com>
2024-06-06 07:50:24 +0700

    Add files via upload

--- pages/pengunjung/riwayat.vue
+++ pages/pengunjung/riwayat.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="container-fluid">
+  <div class="container-fluid table-responsive">
     <div class="row">
       <div class="col-lg-12">
         <h2 class="text-center my-4">RIWAYAT KUNJUNGAN</h2>
         <div class="my-3">menampilkan {{ visitors.length }} dari {{ visitors.length }}</div>
-        <div class="table-responsive">
+        <div class=" table-responsive">
           <table class="table table-bordered border-white text-white">
             <thead>
               <tr>
@@ -52,14 +52,13 @@ onMounted(() => {
 
 <style scoped>
 .container-fluid {
-  background-image: url('@/assets/img/bg-home-kunjungan.jpg');
+  background-image: url('@/assets/img/home.png');
   background-size: cover;
   height: 100vh;
   width: 100%;
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   color:rgb(255, 255, 255);
   color: white;
-  opacity: 70%;
 }
 .btn{
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
